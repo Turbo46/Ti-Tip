e5427be2acc1b39a7006b32181ca952855a0e91f
Alvin Audinata <alvin_audinata@yahoo.com>
2020-11-23 10:08:37 +0700

    Initial commit

---
+++ app/src/main/java/com/rpljumat/ti_tip/DetailTitipan.kt
@@ -0,0 +1,11 @@
+package com.rpljumat.ti_tip
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailTitipan : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_titipan)
+    }
+}
---
+++ app/src/main/java/com/rpljumat/ti_tip/TitipanBaru.kt
@@ -0,0 +1,11 @@
+package com.rpljumat.ti_tip
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TitipanBaru : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_titipan_baru)
+    }
+}
