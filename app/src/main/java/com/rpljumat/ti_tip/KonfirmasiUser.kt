0263d8e99f22cda36603b8dfcd683ddeb422ad61
fatur <faturrahmanstoffel90@gmail.com>
2020-11-24 19:24:40 +0900

    konfirmasi buat user

---
+++ app/src/main/java/com/rpljumat/ti_tip/KonfirmasiUser.kt
@@ -0,0 +1,11 @@
+package com.rpljumat.ti_tip
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class KonfirmasiUser : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_konfirmasi_user)
+    }
+}
