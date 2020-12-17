9129f8a85d00d2d936e6faaf87c2d78a23d9dc44
fatur <faturrahmanstoffel90@gmail.com>
2020-12-17 20:55:44 +0900

    Back riwayat

--- app/src/main/java/com/rpljumat/ti_tip/RiwayatTitipan.kt
+++ app/src/main/java/com/rpljumat/ti_tip/RiwayatTitipan.kt
@@ -1,11 +1,19 @@
 package com.rpljumat.ti_tip
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_help.*
+import kotlinx.android.synthetic.main.activity_riwayat_titipan.*
 
 class RiwayatTitipan : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_riwayat_titipan)
+        tombol_back_riwayat.setOnClickListener {
+            val back = Intent(this, Dashboard::class.java)
+            startActivity(back)
+            finish()
+        }
     }
 }
